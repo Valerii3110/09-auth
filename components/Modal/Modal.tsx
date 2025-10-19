64eccc79b8a1dc20bdc242c5a0434c401f381976
Valeryi Zaletskyi <vzalecki@gmail.com>
2025-10-19 16:16:07 +0300

    fix css

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -1,5 +1,5 @@
 import type { Note } from '@/types/note';
-import css from './NoteList.module.css';
+import css from '../NoteList.module.css';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { deleteNote } from '@/lib/api/clientApi';
 import toast from 'react-hot-toast';
